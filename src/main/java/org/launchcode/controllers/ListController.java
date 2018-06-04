fba336f1b5c6780fdde55fc28631ed2d5d51208f
Tomas Pramod <tomaspramod@yahoo.com>
2018-06-04 18:35:06 -0500

    finished assignment

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -41,6 +41,7 @@ public class ListController {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
+
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
